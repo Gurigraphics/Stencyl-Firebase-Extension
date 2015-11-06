309a6b82d3e1dc9a2d95da0836d75ef3e6b045a3
Gurigraphics <gurigraphics@gmail.com>
2015-11-06 14:21:49 -0200

    Javascript Source
    Javascript Source

---
+++ js/javascriptHX.js
@@ -0,0 +1,470 @@
+var URL_firebase = "https://stencyl.firebaseio.com/";
+
+var srt_01 = "";
+var srt_02 = "";
+var srt_03 = "";
+var srt_04 = "";
+var srt_05 = "";
+var srt_06 = "";
+var srt_07 = "";
+var srt_08 = "";
+var srt_09 = "";
+var srt_10 = "";
+var srt_11 = "";
+var srt_12 = "";
+
+var int_01 = "";
+var int_02 = "";
+var int_03 = "";
+var int_04 = "";
+var int_05 = "";
+var int_06 = "";
+var int_07 = "";
+var int_08 = "";
+var int_09 = "";
+var int_10 = "";
+var int_11 = "";
+var int_12 = "";
+
+var swfObject = document.getElementById("flashObject");
+
+function post_str_01(num1) {
+    str_01 = num1;              
+    var post_str_01 = new Firebase(URL_firebase + "str_01");
+    post_str_01.set({                
+      str_01: str_01,           
+    });
+    update_str_01();
+}
+
+function post_str_02(num2) {
+    str_02 = num2;              
+    var post_str_02 = new Firebase(URL_firebase + "str_02");
+    post_str_02.set({                
+      str_02: str_02,           
+    });
+    update_str_02();
+}
+
+function post_str_03(num3) {
+    str_03 = num3;              
+    var post_str_03 = new Firebase(URL_firebase + "str_03");
+    post_str_03.set({                
+      str_03: str_03,           
+    });
+    update_str_03();
+}
+
+function post_str_04(num4) {
+    str_04 = num4;              
+    var post_str_04 = new Firebase(URL_firebase + "str_04");
+    post_str_04.set({                
+      str_04: str_04,           
+    });
+    update_str_04();
+}
+
+function post_str_05(num5) {
+    str_05 = num5;              
+    var post_str_05 = new Firebase(URL_firebase + "str_05");
+    post_str_05.set({                
+      str_05: str_05,           
+    });
+    update_str_05();
+}
+
+function post_str_06(num6) {
+    str_06 = num6;              
+    var post_str_06 = new Firebase(URL_firebase + "str_06");
+    post_str_06.set({                
+      str_06: str_06,           
+    });
+    update_str_06();
+}
+
+function post_str_07(num7) {
+    str_07 = num7;              
+    var post_str_07 = new Firebase(URL_firebase + "str_07");
+    post_str_07.set({                
+      str_07: str_07,           
+    });
+    update_str_07();
+}
+
+function post_str_08(num8) {
+    str_08 = num8;              
+    var post_str_08 = new Firebase(URL_firebase + "str_08");
+    post_str_08.set({                
+      str_08: str_08,           
+    });
+    update_str_08();
+}
+
+function post_str_09(num9) {
+    str_09 = num9;              
+    var post_str_09 = new Firebase(URL_firebase + "str_09");
+    post_str_09.set({                
+      str_09: str_09,           
+    });
+    update_str_09();
+}
+
+function post_str_10(num10) {
+    str_10 = num10;              
+    var post_str_10 = new Firebase(URL_firebase + "str_10");
+    post_str_10.set({                
+      str_10: str_10,           
+    });
+    update_str_10();
+}
+
+function post_str_11(num11) {
+    str_11 = num11;              
+    var post_str_11 = new Firebase(URL_firebase + "str_11");
+    post_str_11.set({                
+      str_11: str_11,           
+    });
+    update_str_11();
+}
+
+function post_str_12(num12) {
+    str_12 = num12;              
+    var post_str_12 = new Firebase(URL_firebase + "str_12");
+    post_str_12.set({                
+      str_12: str_12,           
+    });
+    update_str_12();
+}
+
+// post int
+
+function post_int_01(num1) {
+    int_01 = num1;              
+    var post_int_01 = new Firebase(URL_firebase + "int_01");
+    post_int_01.set({                
+      int_01: int_01,           
+    });
+    update_int_01();
+}
+
+function post_int_02(num2) {
+    int_02 = num2;              
+    var post_int_02 = new Firebase(URL_firebase + "int_02");
+    post_int_02.set({                
+      int_02: int_02,           
+    });
+    update_int_02();
+}
+
+function post_int_03(num3) {
+    int_03 = num3;              
+    var post_int_03 = new Firebase(URL_firebase + "int_03");
+    post_int_03.set({                
+      int_03: int_03,           
+    });
+    update_int_03();
+}
+
+function post_int_04(num4) {
+    int_04 = num4;              
+    var post_int_04 = new Firebase(URL_firebase + "int_04");
+    post_int_04.set({                
+      int_04: int_04,           
+    });
+    update_int_04();
+}
+
+function post_int_05(num5) {
+    int_05 = num5;              
+    var post_int_05 = new Firebase(URL_firebase + "int_05");
+    post_int_05.set({                
+      int_05: int_05,           
+    });
+    update_int_05();
+}
+
+function post_int_06(num6) {
+    int_06 = num6;              
+    var post_int_06 = new Firebase(URL_firebase + "int_06");
+    post_int_06.set({                
+      int_06: int_06,           
+    });
+    update_int_06();
+}
+
+function post_int_07(num7) {
+    int_07 = num7;              
+    var post_int_07 = new Firebase(URL_firebase + "int_07");
+    post_int_07.set({                
+      int_07: int_07,           
+    });
+    update_int_07();
+}
+
+function post_int_08(num8) {
+    int_08 = num8;              
+    var post_int_08 = new Firebase(URL_firebase + "int_08");
+    post_int_08.set({                
+      int_08: int_08,           
+    });
+    update_int_08();
+}
+
+function post_int_09(num9) {
+    int_09 = num9;              
+    var post_int_09 = new Firebase(URL_firebase + "int_09");
+    post_int_09.set({                
+      int_09: int_09,           
+    });
+    update_int_09();
+}
+
+function post_int_10(num10) {
+    int_10 = num10;              
+    var post_int_10 = new Firebase(URL_firebase + "int_10");
+    post_int_10.set({                
+      int_10: int_10,           
+    });
+    update_int_10();
+}
+
+function post_int_11(num11) {
+    int_11 = num11;              
+    var post_int_11 = new Firebase(URL_firebase + "int_11");
+    post_int_11.set({                
+      int_11: int_11,           
+    });
+    update_int_11();
+}
+
+function post_int_12(num12) {
+    int_12 = num12;              
+    var post_int_12 = new Firebase(URL_firebase + "int_12");
+    post_int_12.set({                
+      int_12: int_12,           
+    });
+    update_int_12();
+}
+
+// Update string memory slots  
+
+function update_str_01() {
+    var update_str_01 = new Firebase(URL_firebase + "str_01"); 
+    update_str_01.on("value", function(data) {
+        var result = data.val().str_01;
+        str_01 = result;
+        swfObject.set_str_01(str_01);             
+    });
+}
+
+function update_str_02() {
+    var update_str_02 = new Firebase(URL_firebase + "str_02");   
+    update_str_02.on("value", function(data) {
+        var result = data.val().str_02;
+        str_02 = result;
+        swfObject.set_str_02(str_02);             
+    });
+}
+
+function update_str_03() {
+    var update_str_03 = new Firebase(URL_firebase + "str_03");    
+    update_str_03.on("value", function(data) {
+        var result = data.val().str_03;
+        str_03 = result;
+        swfObject.set_str_03(str_03);             
+    });
+}
+
+function update_str_04() {
+    var update_str_04 = new Firebase(URL_firebase + "str_04");    
+    update_str_04.on("value", function(data) {
+        var result = data.val().str_04;
+        str_04 = result;
+        swfObject.set_str_04(str_04);             
+    });
+}
+
+function update_str_05() {
+    var update_str_05 = new Firebase(URL_firebase + "str_05");    
+    update_str_05.on("value", function(data) {
+        var result = data.val().str_05;
+        str_05 = result;
+        swfObject.set_str_05(str_05);             
+    });
+}
+
+function update_str_06() {
+    var update_str_06 = new Firebase(URL_firebase + "str_06");    
+    update_str_06.on("value", function(data) {
+        var result = data.val().str_06;
+        str_06 = result;
+        swfObject.set_str_06(str_06);             
+    });
+}
+
+function update_str_07() {
+    var update_str_07 = new Firebase(URL_firebase + "str_07");    
+    update_str_07.on("value", function(data) {
+        var result = data.val().str_07;
+        str_07 = result;
+        swfObject.set_str_07(str_07);             
+    });
+}
+
+function update_str_08() {
+    var update_str_08 = new Firebase(URL_firebase + "str_08");    
+    update_str_08.on("value", function(data) {
+        var result = data.val().str_08;
+        str_08 = result;
+        swfObject.set_str_08(str_08);             
+    });
+}
+
+function update_str_09() {
+    var update_str_09 = new Firebase(URL_firebase + "str_09");    
+    update_str_09.on("value", function(data) {
+        var result = data.val().str_09;
+        str_09 = result;
+        swfObject.set_str_09(str_09);             
+    });
+}
+
+function update_str_10() {
+    var update_str_10 = new Firebase(URL_firebase + "str_10");    
+    update_str_10.on("value", function(data) {
+        var result = data.val().str_10;
+        str_10 = result;
+        swfObject.set_str_10(str_10);             
+    });
+}
+
+function update_str_11() {
+    var update_str_11 = new Firebase(URL_firebase + "str_11");    
+    update_str_11.on("value", function(data) {
+        var result = data.val().str_11;
+        str_11 = result;
+        swfObject.set_str_11(str_11);             
+    });
+}
+
+function update_str_12() {
+    var update_str_12 = new Firebase(URL_firebase + "str_12");    
+    update_str_12.on("value", function(data) {
+        var result = data.val().str_12;
+        str_12 = result;
+        swfObject.set_str_12(str_12);             
+    });
+}
+
+
+// Update integer memory slots
+
+function update_int_01() {
+    var update_int_01 = new Firebase(URL_firebase + "int_01"); 
+    update_int_01.on("value", function(data) {
+        var result = data.val().int_01;
+        int_01 = result;
+        swfObject.set_int_01(int_01);             
+    });
+}
+
+function update_int_02() {
+    var update_int_02 = new Firebase(URL_firebase + "int_02");   
+    update_int_02.on("value", function(data) {
+        var result = data.val().int_02;
+        int_02 = result;
+        swfObject.set_int_02(int_02);             
+    });
+}
+
+function update_int_03() {
+    var update_int_03 = new Firebase(URL_firebase + "int_03");    
+    update_int_03.on("value", function(data) {
+        var result = data.val().int_03;
+        int_03 = result;
+        swfObject.set_int_03(int_03);             
+    });
+}
+
+function update_int_04() {
+    var update_int_04 = new Firebase(URL_firebase + "int_04");    
+    update_int_04.on("value", function(data) {
+        var result = data.val().int_04;
+        int_04 = result;
+        swfObject.set_int_04(int_04);             
+    });
+}
+
+function update_int_05() {
+    var update_int_05 = new Firebase(URL_firebase + "int_05");    
+    update_int_05.on("value", function(data) {
+        var result = data.val().int_05;
+        int_05 = result;
+        swfObject.set_int_05(int_05);             
+    });
+}
+
+function update_int_06() {
+    var update_int_06 = new Firebase(URL_firebase + "int_06");    
+    update_int_06.on("value", function(data) {
+        var result = data.val().int_06;
+        int_06 = result;
+        swfObject.set_int_06(int_06);             
+    });
+}
+
+function update_int_07() {
+    var update_int_07 = new Firebase(URL_firebase + "int_07");    
+    update_int_07.on("value", function(data) {
+        var result = data.val().int_07;
+        int_07 = result;
+        swfObject.set_int_07(int_07);             
+    });
+}
+
+function update_int_08() {
+    var update_int_08 = new Firebase(URL_firebase + "int_08");    
+    update_int_08.on("value", function(data) {
+        var result = data.val().int_08;
+        int_08 = result;
+        swfObject.set_int_08(int_08);             
+    });
+}
+
+function update_int_09() {
+    var update_int_09 = new Firebase(URL_firebase + "int_09");    
+    update_int_09.on("value", function(data) {
+        var result = data.val().int_09;
+        int_09 = result;
+        swfObject.set_int_09(int_09);             
+    });
+}
+
+function update_int_10() {
+    var update_int_10 = new Firebase(URL_firebase + "int_10");    
+    update_int_10.on("value", function(data) {
+        var result = data.val().int_10;
+        int_10 = result;
+        swfObject.set_int_10(int_10);             
+    });
+}
+
+function update_int_11() {
+    var update_int_11 = new Firebase(URL_firebase + "int_11");    
+    update_int_11.on("value", function(data) {
+        var result = data.val().int_11;
+        int_11 = result;
+        swfObject.set_int_11(int_11);             
+    });
+}
+
+function update_int_12() {
+    var update_int_12 = new Firebase(URL_firebase + "int_12");    
+    update_int_12.on("value", function(data) {
+        var result = data.val().int_12;
+        int_12 = result;
+        swfObject.set_int_12(int_12);             
+    });
+}
+
+
